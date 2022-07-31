976bb092715af9cdf2c6e8af47c021646ebce4a2
Bünyamin Elmas <belmas.ben@outlook.com>
2022-07-31 10:28:40 +0300

    CleanBlog8

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  const Blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(Blog);
-});
+// Template Engine
+app.set("view engine", "ejs");
+// Middlewares
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
 
+// Routes
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+  app.get("/about", (req, res) => {
+    res.render("about");
+  });app.get("/add_post", (req, res) => {
+    res.render("add");
+  });app.get("/post", (req, res) => {
+    res.render("index");
+  });
+  
 
 const port = 3000;
 app.listen(port, () => {
