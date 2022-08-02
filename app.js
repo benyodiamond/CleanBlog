9ebac3be9fed04f5eae20e41f18c0f0a12c7bb1a
Bünyamin Elmas <belmas.ben@outlook.com>
2022-08-02 12:26:40 +0300

    ödev10

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
-const mongoose = require('mongoose');
-const post = require('./model/Post');
-
-
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 const app = express();
 
-
-
 //Mognoose
-
-mongoose.connect('mongodb://127.0.0.1:27017/clean-db', {
+mongoose.connect("mongodb://127.0   .0.1:27017/clean-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -25,27 +20,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-    res.render("index");
+
+
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
   });
-  app.get("/about", (req, res) => {
-    res.render("about");
-  });app.get("/add_post", (req, res) => {
-    res.render("add_post");
-  });app.get("/post", (req, res) => {
-    res.render("post");
-  });
+});
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", { post });
+});
 
-  app.post("/posts",  async (req, res) => {
-     await  post.create(req.body);
-     res.redirect("/")
-  });
-  
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
 
-
-
-  
-  
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+});
 
 const port = 3000;
 app.listen(port, () => {
