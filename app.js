4b4391a8ae2521c476c9986443358527efa347c4
Bünyamin Elmas <belmas.ben@outlook.com>
2022-08-04 10:42:42 +0300

    Work5

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Post = require("./models/Post");
 const app = express();
+const methodOverride = require('method-override');
+const postControllers = require('./controllers/postControllers');
+const pageControllers = require('./controllers/pageControllers');
 
 //Mognoose
-mongoose.connect("mongodb://127.0   .0.1:27017/clean-db", {
+mongoose.connect("mongodb://127.0.0.1:27017/clean-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -18,35 +21,24 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
 
 // Routes
+app.get('/', postControllers.getAllPosts);
+app.get('/posts/:id', postControllers.getPost);
+app.post('/posts', postControllers.createPost);
+app.put('/posts/:id', postControllers.updatePost);
+app.delete('/posts/:id', postControllers.deletePost);
 
+app.get('/about', pageControllers.getAboutPage);
+app.get('/add_post', pageControllers.getAddPage);
+app.get('/posts/edit/:id', pageControllers.getEditPage);
 
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("index", {
-    posts,
-  });
-});
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post", { post });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-});
-app.get("/post", (req, res) => {
-  res.render("post");
-});
-
-app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
 
 const port = 3000;
 app.listen(port, () => {
